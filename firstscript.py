31c90fdb6fc9cb547bf3231f73cd342644ef88a9
pfb12 <pfb12@info12.cshl.edu>
2023-10-17 11:14:10 -0400

    Modified first script with f strings

--- firstscript.py
+++ firstscript.py
@@ -3,4 +3,8 @@
 import sys
 
 name = sys.argv[1]
-print("My name:", name)
+color = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
+
+print(f'My name is {name} and I love the color {color}, {activity}, and {animal}.')
